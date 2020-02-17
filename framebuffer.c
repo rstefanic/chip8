422d2ba7481ad66b2bc6f7bdefaead3d50e5f79e
Robert Stefanic <rstefanic72@gmail.com>
2020-02-17 08:43:48 -0600

    Added CPU debug output to framebuffer

--- framebuffer.c
+++ framebuffer.c
@@ -1,7 +1,4 @@
 #include "framebuffer.h"
-#include "memory.h"
-#include "cpu.h"
-#include "bcd.h"
 
 int main()
 {
@@ -23,52 +20,26 @@ int main()
     int start_y = 0;
 
     WINDOW *win = newwin(FRAME_HEIGHT, FRAME_WIDTH, start_y, start_x);
-    box(win, 0, 0);
-    refresh();      // Refresh 
-    wrefresh(win);  // Refresh the window
-    getch();
 
 #if DEBUG
-    mvwprintw(win, 1, 1, "DEBUG Decrement");
-#else
-    mvwprintw(win, 1, 1, "Before Decrement");
-#endif
-
-    mvwprintw(win, 2, 1, "DT: %d", cpu->dt);
-    mvwprintw(win, 3, 1, "ST: %d", cpu->st);
-    mvwprintw(win, 4, 1, "V0: %d", cpu->v0);
-    mvwprintw(win, 5, 1, "V1: %d", cpu->v1);
-
-    mvwprintw(win, 7, 1, "V0 hundreds: %d", bcd_hundreds_at(cpu->v0));
-    mvwprintw(win, 8, 1, "V0 tens: %d", bcd_tens_at(cpu->v0));
-    mvwprintw(win, 9, 1, "V0 ones: %d", bcd_ones_at(cpu->v0));
-
-    mvwprintw(win, 11, 1, "V1 hundreds: %d", bcd_hundreds_at(cpu->v1));
-    mvwprintw(win, 12, 1, "V1 tens: %d", bcd_tens_at(cpu->v1));
-    mvwprintw(win, 13, 1, "V1 ones: %d", bcd_ones_at(cpu->v1));
-    refresh();
-    wrefresh(win);
+    setup_debug_output(win);
+    debug_output(win, cpu);
     getch();
 
     decrement_dt(cpu);
     decrement_st(cpu);
-    mvwprintw(win, 1, 1, "After Decrement");
-    mvwprintw(win, 2, 1, "DT: %d", cpu->dt);
-    mvwprintw(win, 3, 1, "ST: %d", cpu->st);
-    refresh();
-    wrefresh(win);
-
-    // To disable the buffering of typed characters by the TTY driver
-    // cbreak();
-
-    // To suppress the autmoatic echoing of typed characters
-    // noecho();
-
-    // refresh the screen to match what's in memory
+    debug_output(win, cpu);
     getch();
-    refresh();
+#else
+    box(win, 0, 0);
+    mvwprintw(win, 1, 1, "CHIP8");
+    wrefresh(win);
+    getch();
+#endif
 
     endwin(); // exists ncurses
+    free(cpu->stack);
+    free(cpu);
     return 0;
 }
 
@@ -89,3 +60,48 @@ void clear_buffer(Framebuffer *fb)
         fb->buffer[i] = ' ';
     }
 }
+
+#if DEBUG
+void setup_debug_output(WINDOW *win)
+{
+    box(win, 0, 0);
+    refresh();
+    wrefresh(win);
+}
+
+void debug_output(WINDOW *win, CPU *cpu)
+{
+    werase(win);
+
+    mvwprintw(win, 0, 26, "DEBUG CHIP8");
+
+    /* Left Column */
+    mvwprintw(win, 2, 1, "V0: %d", cpu->v0);
+    mvwprintw(win, 3, 1, "V1: %d", cpu->v1);
+    mvwprintw(win, 4, 1, "V2: %d", cpu->v2);
+    mvwprintw(win, 5, 1, "V3: %d", cpu->v3);
+    mvwprintw(win, 6, 1, "V4: %d", cpu->v4);
+    mvwprintw(win, 7, 1, "V5: %d", cpu->v5);
+    mvwprintw(win, 8, 1, "V6: %d", cpu->v6);
+    mvwprintw(win, 9, 1, "V7: %d", cpu->v7);
+    mvwprintw(win, 10, 1, "V8: %d", cpu->v8);
+    mvwprintw(win, 11, 1, "V9: %d", cpu->v9);
+    mvwprintw(win, 12, 1, "VA: %d", cpu->va);
+    mvwprintw(win, 13, 1, "VB: %d", cpu->vb);
+    mvwprintw(win, 14, 1, "VC: %d", cpu->vc);
+    mvwprintw(win, 15, 1, "VD: %d", cpu->vd);
+    mvwprintw(win, 16, 1, "VE: %d", cpu->ve);
+    mvwprintw(win, 17, 1, "VF: %d", cpu->vf);
+
+    mvwprintw(win, 19, 1, "ST: %d", cpu->st);
+    mvwprintw(win, 20, 1, "DT: %d", cpu->dt);
+
+    /* Right Column */
+    // 32 for half of the framebuffer width
+    mvwprintw(win, 2, 32, "SP: %d", cpu->stack->sp);
+    mvwprintw(win, 3, 32, "PC: %d", cpu->stack->pc);
+
+    refresh();
+    wrefresh(win);
+}
+#endif
--- include/framebuffer.h
+++ include/framebuffer.h
@@ -1,4 +1,5 @@
 #include "chip8.h"
+#include "cpu.h"
 
 #define TOTAL_FRAMEBUFFER_SIZE FRAME_WIDTH * FRAME_HEIGHT
 
@@ -9,3 +10,8 @@ typedef struct {
 
 void draw_buffer(Framebuffer *fb);
 void clear_buffer(Framebuffer *fb);
+
+#if DEBUG
+void setup_debug_output(WINDOW *win);
+void debug_output(WINDOW *win, CPU *cpu);
+#endif
