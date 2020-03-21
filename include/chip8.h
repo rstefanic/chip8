580d23c0bee4ad90423e1f0400db3133059e5e4f
Robert Stefanic <rstefanic72@gmail.com>
2020-03-21 10:28:05 -0500

    Added ifndef header wrapper; Added some helper CPP defs for bit shifting

--- include/chip8.h
+++ include/chip8.h
@@ -1,14 +1,24 @@
+#ifndef CHIP8_H
+#define CHIP8_H
+
 #include <ncurses.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
+/* Framebuffer sizes */
 #define FRAME_WIDTH 64
 #define FRAME_HEIGHT 32
 
-/* Size in bits */
+/* Memory Sizes */
 #define BYTE_SIZE 8
 #define END_FONT_SEGMENT 0x080
 #define PROG_DATA_SEGMENT 0x200
 #define MAX_MEMORY 0xFFF
+
+/* Used for bit manipulation on the CPU */
+#define ONE_NIBBLE 4
+#define TWO_NIBBLES 8
+
+#endif /* CHIP8_H not defined */
