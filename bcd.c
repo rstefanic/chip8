386bc30b3796da6cceb5cf5cf370fbff8b774718
Robert Stefanic <rstefanic72@gmail.com>
2020-05-02 08:02:51 -0500

    Fixed BCD module

--- bcd.c
+++ bcd.c
@@ -1,16 +1,16 @@
 #include "bcd.h"
 
-char bcd_hundreds_at(char reg)
+unsigned char bcd_hundreds_at(unsigned char val)
 {
-    return (reg/100) % 10;   
+    return val / 100;
 }
 
-char bcd_tens_at(char reg)
+unsigned char bcd_tens_at(unsigned char val)
 {
-    return (reg/10) % 10;
+    return (val / 10) % 10;
 }
 
-char bcd_ones_at(char reg)
+unsigned char bcd_ones_at(unsigned char val)
 {
-    return reg % 10;
+    return val % 10;
 }
--- include/bcd.h
+++ include/bcd.h
@@ -1,8 +1,8 @@
 #ifndef BCD_H
 #define BCD_H
 
-char bcd_hundreds_at(char reg);
-char bcd_tens_at(char reg);
-char bcd_ones_at(char reg);
+unsigned char bcd_hundreds_at(unsigned char reg);
+unsigned char bcd_tens_at(unsigned char reg);
+unsigned char bcd_ones_at(unsigned char reg);
 
 #endif /* BCD_H not defined */
